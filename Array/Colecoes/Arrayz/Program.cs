d8a40901d6feeba47809c37fb8e145563a3efa1d
Pablo Santos <olbaplegal@gmail.com>
2025-02-06 11:02:56 -0300

    Introdução à arrays

---
+++ Array/Colecoes/Arrayz/Program.cs
@@ -0,0 +1,18 @@
+﻿//declaração do array
+int[] numeros;
+
+//aloquei memória
+numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+string[] nomes = new string[5] { "Maria", "Ana", "João", "Felipe", "Carlos" };
+
+string[] nomes1 = new string[] { "Maria", "Ana", "João", "Felipe", "Carlos" };
+
+string[] nomes2 = { "Maria", "Ana", "João", "Felipe", "Carlos" };
+
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+Console.WriteLine(nomes[2]);
+Console.WriteLine(nomes[3]);
+Console.WriteLine(nomes[4]);
+
