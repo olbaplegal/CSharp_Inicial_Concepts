4fa0565475a066458ae4a20a784c6c24ef5b9493
Pablo Santos <olbaplegal@gmail.com>
2025-02-15 14:58:03 -0300

    List<T>2

--- Array/Colecoes/7List/Program.cs
+++ Array/Colecoes/7List/Program.cs
@@ -1,25 +1,64 @@
-﻿
+﻿////declarar uma coleão do tipo List<T>
+//List<string> lista;
+//lista = new List<string>();
 
-//declarar uma coleão do tipo List<T>
-List<string> lista;
-lista = new List<string>();
+//List<int> lista1 = new List<int>();
 
-List<int> lista1 = new List<int>();
+//var lista2 = new List<float>();
 
-var lista2 = new List<float>();
+//List<double> lista3 = new();
 
-List<double> lista3 = new();
+//List<string> nomes = new();
+//nomes.Add("Paulo");
+//nomes.Add("Maria");
 
-List<string> nomes = new();
-nomes.Add("Paulo");
-nomes.Add("Maria");
+//var lista4 = new List<string>()
+//{
+//    "Paulo", "Maria", "Bia", "Pedro"
+//};
 
-var lista4 = new List<string>()
-{
-    "Paulo", "Maria", "Bia", "Pedro"
-};
+//criação da lista
+var lista = new List<string>() { "Maria", "Ana", "Pedro", "Carlos", "Zé" };
 
+//"Add" adiciona ao final da lista
+lista.Add("Marta");
 
+//"Insert" adiciona no indice escolhido
+lista.Insert(2, "Bia");
+
+//criação de uma collection
+string[] array1 = new string[] { "Dina", "Carlos", "Pedro" };
+
+//adicionando coleção ao final da lista
+lista.AddRange(array1);
+
+//adicionando coleção em um indice expecifico
+lista.InsertRange(1, array1);
+
+//removeu o dado "Dina"
+lista.Remove("Dina");
+
+//removendo dado no indice 4
+lista.RemoveAt(4);
+
+//removendo dados do indice 0 até o 2
+lista.RemoveRange(0, 2);
+
+//acessando um elemento da lista usando indice
+var primeiro = lista[0];
+var quarto = lista[3];
+
+Console.WriteLine(lista[0]);
+Console.WriteLine(lista[3]);
+//console.WriteLine(lista[9]);
+
+//da pra usa o for e o foreach da mesma forma que se usaria em um array
+
+//verifica se tem o valor especificado na lista, retorna Bolean
+var res1 = lista.Contains("Ana");
+
+//remove todos os elementos da lista
+lista.Clear();
 
 Console.ReadKey();
 
--- Array/Colecoes/7List/obj/Debug/net8.0/7List.AssemblyInfo.cs
+++ Array/Colecoes/7List/obj/Debug/net8.0/7List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("7List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f9c11df245d3fcb75f3c893623ad49a19aafcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3408e5b290d035c3f30eb6374dad65dd67385888")]
 [assembly: System.Reflection.AssemblyProductAttribute("7List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("7List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
