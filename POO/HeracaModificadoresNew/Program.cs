ba79f8f04528fe2c86edeb570cc3b4a12872b000
[olbaplegalbackup] <[olbaplegal@gmail.com]>
2025-03-04 12:22:02 -0300

    teste

--- POO/HeracaModificadoresNew/Program.cs
+++ POO/HeracaModificadoresNew/Program.cs
@@ -16,4 +16,4 @@ class Aluno : Pessoa
     public string? Curso { get; set; }
     public new string Saudacao() => $"Oi, meu nome é {Nome} do curso {Curso}"; //saudação substituiu o saudasão da classe base
 }
-//new sinaliza que você realmente quer substituir o método da classe base
+//new sinaliza que você realmente quer substituir o método da classe base.
