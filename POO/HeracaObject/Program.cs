d7bbdb63dffc432fb0461e93a0eb7e72bee21de6
[Pablo Santos] <[olbaplegal@gmail.com]>
2025-03-04 12:05:34 -0300

    teste

--- POO/HeracaObject/Program.cs
+++ POO/HeracaObject/Program.cs
@@ -15,7 +15,7 @@ public class Class2
 
 }
 
-public class ClassDerivada : Class1//, Class2, Struct - não da pra derivar mais de uma classe, todas as classes obj derivam da classe Obj nativa do .NET
+public class ClassDerivada : Class1//, Class2, Struct - não da pra derivar mais de uma classe, todas as classes obj derivam da classe Obj nativa do .NET.
 {
 
 }
