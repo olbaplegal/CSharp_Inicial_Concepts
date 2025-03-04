0f35b9e0b1224d6f108653b7ba5672137712916c
[Pablo Santos] <[olbaplegal@gmail.com]>
2025-03-04 12:04:30 -0300

    adicionado HeracaObject

---
+++ POO/HeracaObject/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.ReadKey();
+
+public struct Struct1
+{
+
+}
+
+public class Class1
+{
+
+}
+
+public class Class2
+{
+
+}
+
+public class ClassDerivada : Class1//, Class2, Struct - não da pra derivar mais de uma classe, todas as classes obj derivam da classe Obj nativa do .NET
+{
+
+}
--- POO/HeracaObject/obj/Debug/net8.0/HeracaObject.AssemblyInfo.cs
+++ POO/HeracaObject/obj/Debug/net8.0/HeracaObject.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HeracaConceitoSintaxe")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HeracaObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c43a44e5d59b22b4ddc9ba5e3d4987e1856482")]
-[assembly: System.Reflection.AssemblyProductAttribute("HeracaConceitoSintaxe")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HeracaConceitoSintaxe")]
+[assembly: System.Reflection.AssemblyProductAttribute("HeracaObject")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HeracaObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
