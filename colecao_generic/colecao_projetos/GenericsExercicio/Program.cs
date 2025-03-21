045ad92a1ec9d70c3a060537eac7e36b59db438d
olbaplegal <olbaplegal@gmail.com>
2025-03-21 10:42:28 -0300

    adicionardo generics exercicio

---
+++ colecao_generic/colecao_projetos/GenericsExercicio/Program.cs
@@ -0,0 +1,38 @@
+﻿ClasseGenerica<int> objInt = new ClasseGenerica<int>();
+
+objInt.Adicionar(10);
+objInt.Adicionar(20);
+objInt.Adicionar(30);
+objInt.Adicionar(40);
+objInt.Adicionar(50);
+
+Console.WriteLine("Classe ClasseGenerica<T> de objetos int\n");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(objInt[i]);
+}
+
+Console.ReadKey();
+
+public class ClasseGenerica<T>//criou uma classe generica usando o parametro de dipo chamado T
+{
+    //definição do array com 5 elementos
+    T[] obj = new T[5];
+    int contador = 0;
+    //adicionar elementos
+    public void Adicionar(T item)
+    {
+         if(contador < 5)
+        {
+            obj[contador] = item;
+        }
+        contador++;
+    }
+    //indexador
+    public T this[int index]
+    {
+        get { return obj[index]; }
+        set { obj[index] = value;  }
+    }
+}
---
+++ colecao_generic/colecao_projetos/colecao_projetos/Program.cs
@@ -0,0 +1,37 @@
+﻿Teste teste = new Teste();
+
+int i1 = 10;
+int i2 = 10;
+
+float f1 = 10;
+float f2 = 10;
+
+string s1 = "10";
+string s2 = "10";
+
+teste.Comparar(i1, i2);
+teste.Comparar(f1, f2);
+teste.Comparar(s1, s2);
+teste.Comparar(i1, f2);
+
+Console.WriteLine();
+
+public class Teste
+{
+    public void Comparar<T1, T2>(T1 p1, T2 p2)
+    {
+        Console.WriteLine($"Os tipos : {p1.GetType()} e {p2.GetType()}\n");
+        var resultado = p1.Equals(p2);
+        Console.WriteLine($"{p1} e {p2} são iguais? {resultado}");
+    }
+    //public void Comparar(float p1, float p2)
+    //{
+    //    var resultado = p1.Equals(p2);
+    //    Console.WriteLine($"{p1} e {p2} são iguais? {resultado}");
+    //}
+    //public void Comparar(string p1, string p2)
+    //{
+    //    var resultado = p1.Equals(p2);
+    //    Console.WriteLine($"{p1} e {p2} são iguais? {resultado}");
+    //}
+}
