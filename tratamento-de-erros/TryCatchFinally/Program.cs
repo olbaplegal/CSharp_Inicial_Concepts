a4bbc539c211625679329133aa7fe3f4dc95ce44
olbaplegal <olbaplegal@gmail.com>
2025-04-05 14:22:23 -0300

    adicionado Try Catch Finally

---
+++ tratamento-de-erros/TryCatchFinally/Program.cs
@@ -0,0 +1,33 @@
+﻿try
+{
+    Console.WriteLine("\ninforme o dividendo");
+    int dividendo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\ninforme o divisor");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    int resultado = (dividendo / divisor);
+    Console.WriteLine($"\n{dividendo} / {divisor} = {resultado}\n");
+}
+catch (FormatException)
+{
+    Console.WriteLine($"\ninforme um valor inteiro");
+    //Console.WriteLine($"\n{ex.Message}");//simples
+    //Console.WriteLine($"\n{ex.Source}");//resumida
+    //Console.WriteLine($"\n{ex.StackTrace}");//pilha
+}
+catch (OverflowException)
+{
+    Console.WriteLine("informe um valor inteiro entre 1 e 999999");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("não existe divisão por zero tente novamente");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+Console.ReadKey();
