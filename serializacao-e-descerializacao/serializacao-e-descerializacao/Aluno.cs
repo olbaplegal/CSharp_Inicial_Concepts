d696bd5f4cb4ea5fd75772f34ffe6407e6411edc
olbaplegal <olbaplegal@gmail.com>
2025-04-06 11:54:42 -0300

    serialização de xml

---
+++ serializacao-e-descerializacao/serializacao-e-descerializacao/Aluno.cs
@@ -0,0 +1,20 @@
+﻿namespace SerializacaoXML
+{
+    public class Aluno
+    {
+        public Aluno(int id, string name, string email, int idade)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Idade = idade;
+        }
+
+        public Aluno() { }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Idade { get; set; }
+    }
+}
---
+++ serializacao-e-descerializacao/serializacao-e-descerializacao/Program.cs
@@ -0,0 +1,18 @@
+﻿using SerializacaoXML;
+using System.Xml.Serialization;
+
+Aluno aluno1 = new Aluno(101, "Maria", "maria@yahoo.com", 17);
+
+string caminhoArquivo = @"D:\Dev\dados-teste\AlunoSerializado.xml";
+
+XmlSerializer serializer = new XmlSerializer(typeof(Aluno));
+
+using(StreamWriter writer = new StreamWriter(caminhoArquivo))
+{
+    serializer.Serialize(writer, aluno1);
+}
+
+Console.WriteLine("objeto serializado para XML com sucesso!");
+Console.ReadKey();
+
+
