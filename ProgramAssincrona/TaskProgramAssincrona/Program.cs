c4e8a0dc454709f89297a421b955115b628b2880
olbaplegal <olbaplegal@gmail.com>
2025-04-08 11:54:18 -0300

    task e await

---
+++ ProgramAssincrona/TaskProgramAssincrona/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("tecle algo para iniciar...");
+Console.ReadKey();
+
+await Aguardar(5); //aguarda a finalização de uma tarefa
+
+Console.WriteLine("já passou 5 segundos...\n");
+Console.WriteLine("fim");
+Console.ReadLine();
+
+Console.ReadKey();
+
+static async Task Aguardar(int tempo) //ultilizando Task para retornar para o await no main
+{
+    Console.WriteLine("iniciando espera...");
+    await Task.Delay(TimeSpan.FromSeconds(tempo)); //await para continuar a para fazer a main roda idependente
+    Console.WriteLine("Fim da espera...");
+}
